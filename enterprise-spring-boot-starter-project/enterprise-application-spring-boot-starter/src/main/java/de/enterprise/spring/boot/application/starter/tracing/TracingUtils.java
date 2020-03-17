ba3333ebdd7cf025ea11bf39bdcae4364644c665
Jonas Keßler <jonas.kessler@kalo.de>
2020-03-17 14:52:12 +0100

    make generation of traceId in TracingUtils configurable

--- enterprise-spring-boot-starter-project/enterprise-application-spring-boot-starter/src/main/java/de/enterprise/spring/boot/application/starter/tracing/TracingUtils.java
+++ enterprise-spring-boot-starter-project/enterprise-application-spring-boot-starter/src/main/java/de/enterprise/spring/boot/application/starter/tracing/TracingUtils.java
@@ -1,21 +1,28 @@
 package de.enterprise.spring.boot.application.starter.tracing;
 
 import java.util.UUID;
+import java.util.function.Function;
 
 import org.slf4j.MDC;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 
+ *
  * @author Malte Geßner
  *
  */
 @Slf4j
 public final class TracingUtils {
 
+	private static Function<String, String> uuidGeneratorFunction = applicationName -> applicationName + "-" + UUID.randomUUID().toString();
+
 	private TracingUtils() {
 
+	}
+
+	public static void setUuidGeneratorFunction(Function<String, String> uuidGeneratorFunction) {
+		TracingUtils.uuidGeneratorFunction = uuidGeneratorFunction;
 	}
 
 	public static String retrieveOrCreate(String traceValue, String applicationName) {
@@ -23,7 +30,7 @@ public final class TracingUtils {
 
 		if (traceId == null) {
 			log.debug("no traceId exist, created new one!");
-			traceId = applicationName + "-" + UUID.randomUUID().toString();
+			traceId = uuidGeneratorFunction.apply(applicationName);
 		}
 
 		return traceId;
